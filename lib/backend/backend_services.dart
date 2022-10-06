96fb2c2683d8592606296b0ef3137f279f520852
modather <modather0ali@gmail.com>
2022-10-06 13:10:53 +0200

    new update

--- lib/backend/backend_services.dart
+++ lib/backend/backend_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
-
+import 'dart:io';
+import 'package:path/path.dart';
 import 'package:http/http.dart' as http;
 
 class BackendServices {
@@ -32,6 +33,40 @@ class BackendServices {
       }
     } catch (e) {
       log("getRequest Error $e");
+    }
+  }
+
+  Future postFileRequest(String url, Map data, File file) async {
+    try {
+      var request = http.MultipartRequest("POST", Uri.parse(url));
+
+      Stream<List<int>> stream = http.ByteStream(file.openRead());
+      int length = await file.length();
+      String? filename = basename(file.path);
+      var multipartFile = http.MultipartFile(
+        "image",
+        stream,
+        length,
+        filename: filename,
+      );
+
+      request.files.add(multipartFile);
+      data.forEach((key, value) {
+        request.fields[key] = value;
+      });
+
+      var sendRequest = await request.send();
+
+      var response = await http.Response.fromStream(sendRequest);
+
+      if (sendRequest.statusCode == 200) {
+        var responseBody = jsonDecode(response.body);
+        return responseBody;
+      } else {
+        log("error statusCode ${sendRequest.statusCode}");
+      }
+    } catch (e) {
+      log("postFileRequest Error $e");
     }
   }
 }
